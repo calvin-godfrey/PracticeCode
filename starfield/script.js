4c02947c437304f006da42f781913fca87890381
Calvin Godfrey <calvinbbcard@gmail.com>
2016-12-21 23:43:45 -0500

    Added a starfield program that just lets you imagine you're flying through space

--- starfield/script.js
+++ starfield/script.js
@@ -1,13 +1,14 @@
 window.onload = function(){
   var c = document.getElementById("canvas");
+  c.width = screen.width;
+  c.height = screen.height;
   var ctx = c.getContext("2d");
   var width = c.width;
-  var height = c.height;
-  var SHIFT = 3;
+  var height = c.height*0.9;
+  var SHIFT = 2;
   var MAX_DIST = Math.sqrt(Math.pow(width/2,2)+Math.pow(height/2,2));
   ctx.fillStyle="#000";
   ctx.fillRect(0,0,width,height);
-  ctx.beginPath();
 
   pointArray = [];
 
@@ -17,6 +18,8 @@ window.onload = function(){
     this.y = height/2;
     this.prevY = this.y;
     this.angle = Math.random()*2*Math.PI;
+    this.speed = SHIFT+Math.random()*16;
+    this.base = 3+Math.random()*6;
   };
 
   point.prototype.getDistance = function(){
@@ -28,20 +31,25 @@ window.onload = function(){
   };
 
   point.prototype.draw = function(){
-    var radius = 5*(this.getOldDistance()/MAX_DIST);
-    ctx.arc(this.preV, this.prevY, radius, 0, Math.PI*2);
-    ctx.fillStyle = "#000";
-    ctx.fill();
+    var radius = 2*this.base*(this.getOldDistance()/MAX_DIST);
+    ctx.beginPath();
+    ctx.moveTo(this.prevX, this.prevY);
+    ctx.arc(this.prevX, this.prevY, radius, 0, Math.PI*2);
     ctx.strokeStyle = "#000";
     ctx.stroke();
+    ctx.fillStyle = "#000";
+    ctx.fill();
+
     if(this.x>0 && this.x<width && this.y>0 && this.y < height)
     {
-      var radius = 1*(this.getDistance()/MAX_DIST);
-      ctx.arc(this.x, this.y, radius, 0, Math.PI*2);
-      ctx.fillStyle = "#FFF";
-      ctx.fill();
-      ctx.strokeStyle = "#FFF";
+      var r_radius = this.base*(this.getDistance()/MAX_DIST);
+      ctx.beginPath();
+      ctx.moveTo(this.x, this.y);
+      ctx.arc(this.x, this.y, r_radius, 0, Math.PI*2);
+      ctx.strokeStyle = "#777";
       ctx.stroke();
+      ctx.fillStyle = "#777";
+      ctx.fill();
       return true;
     }
     else{
@@ -50,8 +58,11 @@ window.onload = function(){
   };
 
   point.prototype.shift = function(){
-      this.x += Math.cos(this.angle)*SHIFT;
-      this.y += Math.sin(this.angle)*SHIFT;
+      this.prevX = this.x;
+      this.prevY = this.y;
+      this.x += Math.cos(this.angle)*this.speed;
+      this.y += Math.sin(this.angle)*this.speed;
+      this.speed *= 1.04;
   };
 
   function addPoint(){
@@ -59,8 +70,9 @@ window.onload = function(){
     pointArray[pointArray.length-1].draw();
   };
 
-  for(var i=0;i<1;i++){
+  for(var i=0;i<200;i++){
     pointArray.push(new point());
+    for(var j=0;j<Math.random()*20;j++){pointArray[i].shift();}
     pointArray[i].draw();
   }
 
@@ -69,7 +81,7 @@ window.onload = function(){
       var current = pointArray[i];
       current.shift();
       if(current.draw()==false){
-        pointArray.pop(i);
+        pointArray.splice(i,1);
         i--;
         addPoint();
       }
