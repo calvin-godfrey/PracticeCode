0770c634eb5fe4a993d37309f49e189a42459a74
Calvin Godfrey <calvinbbcard@gmail.com>
2017-01-23 13:15:02 -0500

    Good stopping point, lots of features for pretty animations

--- circle/script.js
+++ circle/script.js
@@ -7,6 +7,31 @@ window.onload = function(){
   var Y_CENTER = canvas.height/2;
   var INNER_RADIUS = RADIUS/2;
   var ctx = canvas.getContext("2d");
+  ctx.translate(0.5,0.5);
+  window.requestAnimFrame = (function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
 
   Point = function(x, y, angle, life){
     this.x = x;
@@ -19,7 +44,6 @@ window.onload = function(){
     this.x = 200*Math.cos(this.life);
     this.y = 0;
     this.life = this.life + Math.PI/72;
-    this.farX = this.x*1.5;
   }
 
   Point.prototype.draw = function(){
@@ -27,7 +51,7 @@ window.onload = function(){
     ctx.translate(X_CENTER, Y_CENTER);
     ctx.rotate(this.angle);
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 5, 0, Math.PI*2);
+    ctx.arc(this.x, this.y,2, 0, Math.PI*2);
     ctx.strokeStyle = "#000";
     ctx.stroke();
     ctx.fillStyle = "#000";
@@ -40,41 +64,96 @@ window.onload = function(){
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.arc(X_CENTER, Y_CENTER, RADIUS, 0, Math.PI*2);
-    ctx.strokeStyle = "#F00";
+    ctx.strokeStyle = "#000";
     ctx.stroke();
+    if(drawTracks){
+    for(var i=0;i<points.length;i++){
+        var j = i*Math.PI/points.length;
+        ctx.beginPath();
+        ctx.moveTo(X_CENTER+200*Math.cos(j), Y_CENTER+200*Math.sin(j));
+        ctx.lineTo(X_CENTER-200*Math.cos(j), Y_CENTER-200*Math.sin(j));
+        ctx.stroke();
+      }
+    }
   }
 
   function drawLine(){
-    ctx.save();
-    ctx.translate(X_CENTER, Y_CENTER);
-    ctx.rotate(points[0].angle);
-    ctx.beginPath();
-    ctx.moveTo(points[0].farX, points[0].x);
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "#0FF";
-    ctx.restore();
-    ctx.save();
-    ctx.translate(X_CENTER, Y_CENTER);
-    ctx.rotate(points[4].angle);
-    ctx.lineTo(points[4].x, points[4].y);
-    ctx.stroke();
-    ctx.restore();
+    for(var i=0;i<Math.floor(points.length/2);i++){
+      ctx.save();
+      ctx.translate(X_CENTER, Y_CENTER);
+      ctx.rotate(points[i].angle);
+      ctx.beginPath();
+      ctx.moveTo(points[i].x, points[i].y);
+      ctx.restore();
+      ctx.save();
+      ctx.translate(X_CENTER, Y_CENTER);
+      ctx.rotate(points[Math.floor(points.length/2)+i].angle);
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = "#F00";
+      ctx.lineTo(points[Math.floor(points.length/2)+i].x, 0);
+      ctx.stroke();
+      ctx.restore();
+    }
   }
 
-  var points = [];
-  for(var i=0;i<8;i++){
-    points.push(new Point(X_CENTER, Y_CENTER, i*Math.PI/8, 12.964*i)); //Legit last one is a magic number. Now clue why it is what it is
-  }
-
+  var points;
+  var drawLines = false;
+  var drawTracks = false;
+  var restart = false;
+  var stopped = false;
+  var doloop = undefined;
   function loop(){
     drawBackground();
     for(var i=0;i<points.length;i++){
       points[i].calcPoint();
       points[i].draw();
     }
-    drawLine();
-    requestAnimationFrame(loop);
+    if(drawLines)drawLine();
+    if(restart){stopped=true;return;}
+    if(!restart)var doloop = window.requestAnimationFrame(loop);
   }
-  requestAnimationFrame(loop);
+
+  function stop(){
+    if(doloop){
+      cancelRequestAnimationFrame(doloop);
+      doloop = undefined;
+    }
+  }
+
+  function start(){
+    if(!doloop)loop();
+  }
+
+  document.getElementById("start").addEventListener("mousedown", function(event){
+    stop();
+    restart = true;
+    var numPoints = document.getElementById("points").value;
+    var magic = document.getElementById("magic").value;
+    points = [];
+    for(var i=0;i<numPoints;i++){
+      points.push(new Point(X_CENTER, Y_CENTER, i*Math.PI/numPoints, i*magic/numPoints)); //Legit last one is a magic number. Now clue why it is what it is
+    }
+    restart = false;
+    start();
+  });
+
+  var p = setInterval(function(){
+    document.getElementById("selected").innerHTML = document.getElementById("points").value;
+  }, 500);
+
+  var l = setInterval(function(){
+    if(document.getElementById("lines").checked)drawLines = true;
+    else{drawLines = false;}
+  }, 500);
+
+  var t = setInterval(function(){
+    if(document.getElementById("track").checked)drawTracks = true;
+    else{drawTracks = false;}
+  }, 500);
+
+  document.getElementById("stop").addEventListener("mousedown", function(event){
+    stop();
+    restart = true;
+  });
 
 }
