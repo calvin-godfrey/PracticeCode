89883a209c5f4779015dc7df8bea5e79bb7ac3c9
= <calvinbbcard@gmail.com>
2017-03-11 15:25:14 -0500

    More features to the SIS_API

--- SIS_API.py
+++ SIS_API.py
@@ -1,17 +1,20 @@
-import bs4, mechanize
+import bs4, mechanize, sys, argparse, getpass, os.path
+
 url = "https://sisstudent.fcps.edu"
 br = mechanize.Browser()
-login_info = open("SIS_DATA.txt", "r") #So that y'all don't know my information
-login_info = login_info.readlines()
-username = login_info[0].strip() #cuts of unnecessary stuff
-password = login_info[1].strip()
+parser = argparse.ArgumentParser(description="Will display user's grade book information from SIS")
+parser.add_argument('-g', '--grade', help="Table without the grades column", action="store_true")
+parser.add_argument('-p', '--period', help="Table without the periods column", action="store_true")
+parser.add_argument('-c', '--course', help="Table without the courses column", action="store_true")
+parser.add_argument('-t', '--teacher', help="Table without the teachers column", action="store_true")
+parser.add_argument('-r', '--room', help="Table without the room number column", action="store_true")
 
-def display_info(p, c, r, t, g):
+def display_info(p, p_show, c, c_show, r, r_show, t, t_show, g, g_show):
     BUFFER_SIZE = 18
-    print "Period".ljust(BUFFER_SIZE)+ "|" + "Course".ljust(BUFFER_SIZE) + "|" + "Room".ljust(BUFFER_SIZE-9) + "|" + "Teacher".ljust(BUFFER_SIZE) + "|Grade" #Room is shorter because it doesn't need all those
-    print "-"*75                                                                                                                                             #Characters and I needed to save space
+    print "Period".ljust(BUFFER_SIZE)*int(p_show)+ "|Course".ljust(BUFFER_SIZE)*int(c_show) + "|Room".ljust(BUFFER_SIZE-9)*int(r_show) + "|Teacher".ljust(BUFFER_SIZE)*int(t_show) + "|Grade"*int(g_show)
+    print "-"*75
     for i in range(len(p)): #They should all be the same length
-        print p[i].ljust(BUFFER_SIZE) + "|" + c[i].ljust(BUFFER_SIZE) + "|" + r[i].ljust(BUFFER_SIZE-9) + "|" + t[i].ljust(BUFFER_SIZE) + "|" + g[i].ljust(BUFFER_SIZE)
+        print (p[i].ljust(BUFFER_SIZE) + "|")*int(p_show) + (c[i].ljust(BUFFER_SIZE) + "|")*int(c_show) + (r[i].ljust(BUFFER_SIZE-9) + "|")*int(r_show) + (t[i].ljust(BUFFER_SIZE) + "|")*int(t_show) + (g[i].ljust(BUFFER_SIZE))*int(g_show)
 
 def save_grades(grades): #To see if any are updated later on
     open("grades.txt", "w").close() #Make sure the file is empty
@@ -25,14 +28,25 @@ def save_grades(grades): #To see if any are updated later on
 def load_grades():
     return open("grades.txt", "r").readlines()
 
-def main():
+def main(disp_p, disp_c, disp_r, disp_t, disp_g):
+    if os.path.isfile("SIS_DATA.txt"): #Cause I don't want to type in my password every time
+        login_info = open("SIS_DATA.txt", "r").readlines()
+        username = login_info[0].strip()
+        password = login_info[1].strip()
+    else:
+        username = raw_input("Type in your username:\n> ")
+        print "Now enter your password: "
+        password = getpass.getpass()
     period, courses, room_numbers, teachers, grade = [], [], [], [], []
     br.open(url)
     br.select_form(name="Form1")
     br.form["username"] = username #Gotten from file saved locally
     br.form["password"] = password
     req = br.submit()
-    grade_book = br.follow_link(text_regex=r"Grade Book") #This is the text for the link to the grades
+    try:
+        grade_book = br.follow_link(text_regex=r"Grade Book") #This is the text for the link to the grades
+    except:
+        raise ValueError("Username/Password Combo Unsuccessful")
     parsed = bs4.BeautifulSoup(grade_book.read(), 'lxml')
     rows = parsed.select("table.info_tbl")[0].find_all("tr") #List of all the rows that contain information
     ignore = False
@@ -60,8 +74,14 @@ def main():
                         grade.append("*"+td.text)
                     else:
                         grade.append(td.text)
-    display_info(period, courses, room_numbers, teachers, grade)
+    display_info(period, disp_p, courses, disp_c, room_numbers, disp_r, teachers, disp_t, grade, disp_g)
     save_grades(grade)
 
 if __name__ == "__main__":
-    main()
+    args = parser.parse_args()
+    r = (args.room)
+    g = (args.grade)
+    p = (args.period)
+    c = (args.course)
+    t = (args.teacher)
+    main(p, c, r, t, g)
