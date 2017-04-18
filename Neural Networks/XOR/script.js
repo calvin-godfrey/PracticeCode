b1bd77acff9e6e091d024bb3b81f1a3e0da083fb
Calvin Godfrey <calvinbbcard@gmail.com>
2017-04-18 15:32:16 -0400

    Has everything needed, but doesn't work

--- Neural Networks/XOR/script.js
+++ Neural Networks/XOR/script.js
@@ -15,14 +15,15 @@ window.onload = function(){
   Neuron.prototype.initialize = function(n){ //where n is the number of neurons in the previous layer, this is only done for the initial generation
     this.weights = [];
     for(var i=0;i<n;i++){
-      this.weights.push(Math.floor(-10*Math.random() + 5));
+      this.weights.push((-10*Math.random() + 5));
     }
     if(this.isInput)return; //input neuron does not need bias
-    this.bias = Math.floor(-10*Math.random() + 5); //Random bias value
+    this.bias = (-10*Math.random() + 5); //Random bias value
   }
 
   Neuron.prototype.setValues = function(weights, bias){ //This is done for all generations after the first
-    this.weights = weights;
+    if(this.isInput)this.weights = weights[0];
+    if(!this.isInput)this.weights = weights;
     if(this.isInput)return;
     this.bias = bias;
   }
@@ -57,12 +58,12 @@ window.onload = function(){
     }
     if(isInitial){
       this.ready = true;
-      for(var j=0;j<layers[0].length;i++){
+      for(var j=0;j<this.layers[0].length;j++){
         this.layers[0][j].initialize(1); //only one weight from the input
       }
-      for(var i=1;i<layers.length;i++){ //Skip the input layer
-        for(var j=0;j<layers[i].length;j++){ //jagged array
-          this.layers[i][j].initialize(layers[i-1].length); //random weight array and values
+      for(var i=1;i<this.layers.length;i++){ //Skip the input layer
+        for(var j=0;j<this.layers[i].length;j++){ //jagged array
+          this.layers[i][j].initialize(this.layers[i-1].length); //random weight array and values
         }
       }
     }
@@ -82,6 +83,7 @@ window.onload = function(){
   }
 
   Network.prototype.process = function(input){
+    if(!this.ready)return;
     for(var i=0;i<input.length;i++){
       input[i] *= this.layers[0][i].weights[0]; //Multiplies input by the weight of each input neuron
     }
@@ -95,11 +97,101 @@ window.onload = function(){
     return input;
   }
 
-  function main(){
-    var network = new Network([2, 1, 1], false); //Two input neurons, one hidden neuron, one output neuron
+  function eliminate(networks){ //networks should be sorted by fitness
+    for(var i=0;i<networks.length;i++){
+      var chance = Math.random()*(i*0.5+10);
+      if(chance > 9.5){
+        networks[i] = 0;
+      }
+    }
+    return networks.filter(function(a){return a!=0}); //Remove all the 0's
+  }
 
-    network.setValues([[[2], [2]], [[[2, 1], 1]], [[[2], 1]]]);
-    console.log(network.process([1, 1]));
+  function populate(networks, size){
+    var len = networks.length; //So that we only take values from initial population, not newly generated networks
+    while(networks.length < size){
+      var index1 = Math.floor(Math.random()*len);
+      var index2 = index1;
+      while(index2 == index1){
+        index2 = Math.floor(Math.random()*len); //Make sure the same network isn't chosen twice
+      }
+      networks.push(cross(networks[index1], networks[index2]));
+    }
+    return networks;
+  }
+
+  function cross(network1, network2){
+    var size = [];
+    for(var i=0;i<network1.layers.length;i++){
+      size.push(network1.layers[i].length);
+    }
+    var child = new Network(size, false); //should be the same size
+    var newValues = [];
+    for(var i=0;i<network1.layers.length;i++){ //lengths should be equal
+      newValues.push([]);
+      var layer1 = network1.layers[i];
+      var layer2 = network2.layers[i];
+      for(var j=0;j<layer1.length;j++){
+        newValues[i].push([[]]); //array consisting of another array (weights), later a number (bias)
+        var neuron1 = layer1[j];
+        var neuron2 = layer2[j];
+        for(var k=0;k<neuron1.weights.length;k++){
+          var weight1 = neuron1.weights[k];
+          var weight2 = neuron2.weights[k];
+          newValues[i][j][0].push((weight1+weight2)/2 + (-1*Math.random()+0.5)); //Keep random ammount
+        }
+        if(i!=0){
+          var bias1 = neuron1.bias;
+          var bias2 = neuron2.bias;
+          newValues[i][j].push((bias1+bias2)/2 + (-1*Math.random()+0.5));
+        }
+      }
+    }
+    console.log(newValues);
+    child.setValues(newValues);
+    return child;
+  }
+
+  function generation(networks){
+    for(var i=0;i<networks.length;i++){
+      var network = networks[i];
+      var error = 0;
+      for(var j=0;j<inputData.length;j++){
+        var x = network.process(inputData[j]);
+        error += Math.abs(outputData[j]-x[0]);
+      }
+      network.fitness = 4-error; //Smaller error means higher fitness
+    }
+    return networks;
+  }
+
+  function main(){
+    //var network = new Network([2, 1, 1], false); //Two input neurons, one hidden neuron, one output neuron
+    var networks = [];
+
+    for(var i=0;i<50;i++){
+      networks.push(new Network([2, 1, 1], true));
+    }
+    for(var z=0;z<10;z++){
+      console.log("--------------------");
+      networks = generation(networks);
+
+      networks.sort(function(a, b){
+        return b.fitness-a.fitness; //Arranged in descending order
+      });
+
+      for(var i=0;i<networks.length;i++){
+        console.log(networks[i].fitness);
+      }
+
+      networks = eliminate(networks);
+
+      networks = populate(networks, 50);
+    }
+
+
+    //network.setValues([[[2], [2]], [[[2, 1], 1]], [[[2], 1]]]);
+    //console.log(network.process([1, 1]));
   }
 
   document.getElementById("start").addEventListener("mousedown", function(event){
